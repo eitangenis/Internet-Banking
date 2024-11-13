0f45762afa3ddb7c8db64ead65c7299a99205046
Anton Genis <anton.genis1@gmail.com>
2024-11-13 16:22:28 +0200

    added

--- InternetBanking.Core.Application/ViewModels/Beneficiary/SaveBeneficiaryViewModel.cs
+++ InternetBanking.Core.Application/ViewModels/Beneficiary/SaveBeneficiaryViewModel.cs
@@ -7,7 +7,7 @@ namespace InternetBanking.Core.Application.ViewModels.Beneficiary
         public int? Id { get; set; }
         public string? UserOwnerId { get; set; }
 
-        [Display(Name = "Numero de cuenta")]
+        [Display(Name = "Account Number")]
         public string AccountNumberBeneficiary { get; set; }
         public bool HasError { get; set; }
         public string? Error { get; set; }
