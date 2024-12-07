c80608c9361f052f86a6ea0a870a03a52ffb63a5
Anton Genis <anton.genis1@gmail.com>
2024-12-07 23:03:29 +0200

    more controllers

---
+++ InternetBanking/Controllers/DashboardsController.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AspnetCoreMvcFull.Controllers;
+
+public class DashboardsController : Controller
+{
+  public IActionResult Index() => View();
+}
---
+++ InternetBanking/Controllers/ExtendedUiController.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AspnetCoreMvcFull.Controllers;
+
+public class ExtendedUiController : Controller
+{
+  public IActionResult PerfectScrollbar() => View();
+  public IActionResult TextDivider() => View();
+}
